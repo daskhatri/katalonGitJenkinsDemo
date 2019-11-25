9216cb6ca33432fc93e197e6ef09364a0deb73d2
daskhatri <blubd.softtech@gmail.com>
2019-11-26 02:39:18 +0500

    Created Test1 with new official ID account.

---
+++ Test Suites/TestSuite2611.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite2611</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>aebca6df-ded2-4ce6-bd1f-670e15bab2ba</testSuiteGuid>
+</TestSuiteEntity>
